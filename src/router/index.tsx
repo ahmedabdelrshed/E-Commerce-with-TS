5acc998c2bd6bc9ed93d36e19ba6eb9367604e66
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-01-20 08:15:42 +0200

    create main routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,13 +3,22 @@ import {
   createRoutesFromElements,
   Route,
 } from "react-router";
+// Layouts
 import MainLayout from "../layouts/MainLayout/MainLayout";
+// Pages
+import Home from "@pages/Home";
+import Categories from "@pages/Categories";
+import Product from "@pages/Products";
+import AboutUs from "@pages/AboutUs";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<MainLayout/>}>
-        <Route index element={<h1>hhhhhhh</h1>} />
+        <Route index element={<Home/>} />
+        <Route path="categories" element={<Categories/>} />
+        <Route path="products:prefix" element={<Product/>} />
+        <Route path="about-us" element={<AboutUs/>} />
       </Route>
     </>
   )
