5f91141a0c9a3be55ec45c1512adfd34321eb914
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-01-21 21:43:24 +0200

    save cartItems in loaclStorage using reactPersistor

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppRouter from "./router/AppRouter";
 import { Provider } from "react-redux";
-import { store } from "@store/store";
+import { persisterStore, store } from "@store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <AppRouter />
+    <PersistGate loading={null} persistor={persisterStore}>
+      <AppRouter />
+    </PersistGate>
   </Provider>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,14 +2,22 @@ import { configureStore } from '@reduxjs/toolkit'
 import categoriesSlice from './categories/categoriesSlice'
 import productsSlice from './products/productsSlice'
 import cart from './cart/cartSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+
+const persistCartConfig = {
+    key: 'cart',
+    storage
+}
+const persistCart = persistReducer(persistCartConfig, cart)
 export const store = configureStore({
     reducer: {
         categoriesSlice,
         productsSlice,
-        cart
+        persistCart
     },
 })
-
+export const persisterStore = persistStore(store)
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
