868236d78c63c318f274892e83a703293c043ab1
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-01-22 14:33:46 +0200

    animation for cartIcon and addToCart action using Debouncing Techinque

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -2,13 +2,26 @@ import Logo from "@assets/svg/cart.svg?react";
 import styles from "./basketStyles.module.css";
 import { useAppSelector } from "@store/hooks";
 import { getCartTotalQuantity } from "@store/cart/cartSlice";
-const { basketQuantity, basketContainer } = styles;
+import { useEffect, useState } from "react";
+const { basketQuantity, basketContainer, pumpCartQuantity } = styles;
 const HeaderBasket = () => {
+  const [isAnimated, setISAnimated] = useState(false);
   const totalItemsQuantity = useAppSelector(getCartTotalQuantity);
+  const basketStyles = `${basketQuantity} ${
+    isAnimated ? pumpCartQuantity : ""
+  }`;
+  useEffect(() => {
+    if (!totalItemsQuantity) return;
+    setISAnimated(true);
+    const debounce = setTimeout(() => {
+      setISAnimated(false);
+    }, 300);
+    return () => clearTimeout(debounce);
+  }, [totalItemsQuantity]);
   return (
     <div className={basketContainer}>
       <Logo title="basket-logo" />
-      <div className={basketQuantity}>{totalItemsQuantity} </div>
+      <div className={basketStyles}>{totalItemsQuantity} </div>
     </div>
   );
 };
--- src/components/eCommerce/Product/Product.tsx
+++ src/components/eCommerce/Product/Product.tsx
@@ -1,16 +1,26 @@
-import { Button } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 import styles from "./productStyle.module.css";
 import { IProduct } from "@customTypes/index";
 import { useAppDispatch } from "@store/hooks";
 import { addToCart } from "@store/cart/cartSlice";
+import { useEffect, useState } from "react";
 const { product, productImg } = styles;
 
-const Product = ({id, img, price, title }: IProduct) => {
-    const dispatch = useAppDispatch()
-    // Handlers
-    const onAddToCart = () => { 
-        dispatch(addToCart(id))
-    }
+const Product = ({ id, img, price, title }: IProduct) => {
+  const dispatch = useAppDispatch();
+  const [isBtnDisabled, setIsBtnDisabled] = useState(false);
+  useEffect(() => {
+    if (!isBtnDisabled) return;
+    const debounce = setTimeout(() => {
+      setIsBtnDisabled(false);
+    }, 300);
+    return () => clearTimeout(debounce);
+  }, [isBtnDisabled]);
+  // Handlers
+  const onAddToCart = () => {
+    dispatch(addToCart(id));
+    setIsBtnDisabled(true);
+  };
   return (
     <div className={product}>
       <div className={productImg}>
@@ -18,8 +28,19 @@ const Product = ({id, img, price, title }: IProduct) => {
       </div>
       <h2 title={title}>{title}</h2>
       <h3>{price}</h3>
-      <Button variant="info" style={{ color: "white" }} onClick={onAddToCart}>
-        Add to cart
+      <Button
+        variant="info"
+        style={{ color: "white" }}
+        onClick={onAddToCart}
+        disabled={isBtnDisabled}
+      >
+        {isBtnDisabled ? (
+          <>
+            <Spinner animation="border" size="sm" /> Loading...
+          </>
+        ) : (
+          "Add to cart"
+        )}
       </Button>
     </div>
   );
