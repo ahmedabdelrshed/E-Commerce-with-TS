c3c08f26f7bb5a894ea778bc08c8669c22790eef
ahmedabdelrshed <104847521+ahmedabdelrshed@users.noreply.github.com>
2025-01-30 21:35:54 +0200

    typescript alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import tsConfigPaths from "vite-tsconfig-paths";
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsConfigPaths()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@customTypes": path.resolve(__dirname, "./src/types"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+
+    }
+
+  },
+  plugins: [react(), svgr()],
 })
